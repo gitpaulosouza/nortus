ee950b8355662a04cba273dfe23f6be0ce711aae
Paulo Gabriel Andrade de Souza <paulo.gabriel.souza@accenture.com>
2026-02-12 06:00:34 -0300

    chore(app-logo): Add the nortus app logo

--- lib/src/features/auth/presentation/widgets/login_bottom_sheet.dart
+++ lib/src/features/auth/presentation/widgets/login_bottom_sheet.dart
@@ -42,7 +42,7 @@ class _LoginBottomSheetContent extends StatelessWidget {
           SnackbarHelper.showError(
             context,
             'Erro ao autenticar',
-            subtitle: state.errorMessage!,
+            subtitle: 'Não foi possível realizar a autenticação.',
           );
         } else if (state.isRegistrationSuccess) {
           SnackbarHelper.showSuccess(
@@ -53,7 +53,7 @@ class _LoginBottomSheetContent extends StatelessWidget {
 
           Future.delayed(const Duration(milliseconds: 500), () {
             if (context.mounted) {
-              context.read<AuthBloc>().add(AuthModeChanged(AuthFormMode.login));
+              context.read<AuthBloc>().add(AuthFormCleared());
             }
           });
         } else if (state.isSuccess) {
